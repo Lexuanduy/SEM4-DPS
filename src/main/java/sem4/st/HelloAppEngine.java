57154f178bfd7893c8f3202ff950b9ccd4247bba
Lexuanduy <lexuanduy07@gmail.com>
2019-05-20 21:02:00 +0700

    create packages, Login servlet and page

--- src/main/java/sem4/st/HelloAppEngine.java
+++ src/main/java/sem4/st/HelloAppEngine.java
@@ -1,6 +1,10 @@
 package sem4.st;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,7 +20,21 @@ public class HelloAppEngine extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException {
-
+	  Connection connection = null;
+	try {
+		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sem4?useUnicode=true&characterEncoding=utf-8", "root", "");
+		String sql = "Insert into account (userName, password) values (?, ?)";
+	      PreparedStatement ps = connection.prepareStatement(sql);
+	      ps.setString(1, "aaa");
+	      ps.setString(2, "bbb");
+	      ps.execute();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+      
+     
+      System.out.println("Insert success!");
     response.setContentType("text/plain");
     response.setCharacterEncoding("UTF-8");
 
