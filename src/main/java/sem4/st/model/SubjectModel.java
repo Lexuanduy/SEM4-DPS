bf76fc950d276cad99bad3ed9cd90192545dc2c3
phuocding <phuocding@gmail.com>
2019-05-27 05:51:51 +0700

    manipulate with subject, need try to surf path on web

---
+++ src/main/java/sem4/st/model/SubjectModel.java
@@ -0,0 +1,123 @@
+package sem4.st.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import sem4.st.entities.Subject;
+
+public class SubjectModel {
+	static Logger logger = Logger.getLogger(AccountModel.class.getName());
+	public static boolean createSubject(Subject subject) throws SQLException {
+		String sql = "INSERT INTO subject (name, status, createdAt, updatedAt) VALUES (?, ?, ?, ?)";
+		try {
+			Connection cnn = DBConnection.getInstance().getConnection();
+			PreparedStatement ps = cnn.prepareStatement(sql);
+			ps.setString(10, subject.getName());
+			ps.setInt(11, subject.getStatus());
+			ps.setLong(12, subject.getCreatedAt());
+			ps.setLong(13, subject.getUpdatedAt());
+			ps.execute();
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			logger.info(e.getMessage());
+		}
+		return false;
+	}
+	
+	public static List<Subject> listAllSubject() throws SQLException {
+		List<Subject> listSubjects = new ArrayList<>();
+        
+        String sql = "SELECT * FROM subject";
+		try {
+			Connection cnn = DBConnection.getInstance().getConnection();
+			PreparedStatement ps = cnn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery(sql);
+			while(rs.next()) {
+				Subject subject = new Subject();
+				String name = rs.getString("name");
+				int status = rs.getInt("status");
+				long createdAt = rs.getLong("createdAt");
+				long updatedAt = rs.getLong("updatedAt");
+				subject.setCreatedAt(createdAt);
+				subject.setName(name);
+				subject.setStatus(status);
+				subject.setUpdatedAt(updatedAt);
+				listSubjects.add(subject);
+			}
+			return listSubjects;
+		} catch (SQLException e) {
+			logger.info(e.getMessage());
+		}
+		return null;
+	}
+	
+	public static boolean updateSubject(Subject subject) throws SQLException {
+        String sql = "UPDATE subject SET name = ?, createdAt = ?, updatedAt = ?";
+        sql += " WHERE id = ?";
+        try {
+        	Connection cnn = DBConnection.getInstance().getConnection();
+        	PreparedStatement ps = cnn.prepareStatement(sql);
+        	ps.setString(1, subject.getName());
+        	ps.setInt(2, subject.getStatus());
+        	ps.setLong(3, subject.getCreatedAt());
+			ps.setLong(4, subject.getUpdatedAt());
+			ps.setString(4, subject.getId());
+			ps.execute();
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			logger.info(e.getMessage());
+		}
+        return false;
+    }
+	
+	public static boolean deleteSubject(String id) throws SQLException {
+        String sql = "DELETE FROM subject where id = ?";
+        
+        try {
+        	Connection cnn = DBConnection.getInstance().getConnection();
+        	PreparedStatement ps = cnn.prepareStatement(sql);
+        	ps.setString(1, id);
+        	ps.execute();
+        	return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			logger.info(e.getMessage());
+		}    
+        return false;
+    }
+	
+	public static Subject SearchById(String id) throws SQLException {
+		String sql = "SELECT id,name,createdAt,updatedAt from subject where id =?";
+		Subject subject = null;
+		try {
+			Connection cnn = DBConnection.getInstance().getConnection();
+        	PreparedStatement ps = cnn.prepareStatement(sql);
+        	ps.setString(1, id);
+        	System.out.println(ps);
+        	ResultSet rs = ps.executeQuery();
+        	
+        	while (rs.next()) {
+        		String name = rs.getString("name");
+        		int status = rs.getInt("status");
+        		long createdAt = rs.getLong("createdAt");
+				long updatedAt = rs.getLong("updatedAt");
+				subject = new Subject();
+				subject.setCreatedAt(createdAt);
+				subject.setName(name);
+				subject.setStatus(status);
+				subject.setUpdatedAt(updatedAt);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			logger.info(e.getMessage());
+		}
+		return subject;
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/subjects/FormSubject.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Subject Form Application</title>
+</head>
+<body>
+	<div align="center">
+		<h1>User Management</h1>
+		<h2>
+			<a href="new">Add New Subject</a> &nbsp;&nbsp;&nbsp; <a href="list">List
+				All Subject</a>
+
+		</h2>
+	</div>
+	<div align="center">
+		<c:if test="${subject != null}">
+			<form action="update" method="post">
+		</c:if>
+		<c:if test="${subject == null}">
+			<form action="insert" method="post">
+		</c:if>
+		<table border="1" cellpadding="5">
+			<caption>
+				<h2>
+					<c:if test="${subject != null}">
+               Edit User
+              </c:if>
+					<c:if test="${subject == null}">
+               Add New Subject
+              </c:if>
+				</h2>
+			</caption>
+			<c:if test="${subject != null}">
+				<input type="hidden" name="id" value="<c:out value='${subject.id}' />" />
+			</c:if>
+			<tr>
+				<th>User Name:</th>
+				<td><input type="text" name="name" size="45"
+					value="<c:out value='${subject.name}' />" /></td>
+			</tr>
+			<tr>
+				<td colspan="2" align="center"><input type="submit"
+					value="Save" /></td>
+			</tr>
+		</table>
+		</form>
+	</div>
+
+</body>
+</html>
