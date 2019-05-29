046b69b333e1874717e61e360ca101d0cf6764fe
phuocding <phuocding@gmail.com>
2019-05-30 05:08:20 +0700

    refactor subject servlet with view

---
+++ src/main/java/sem4/st/controller/SubjectServlet.java
@@ -0,0 +1,125 @@
+package sem4.st.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sem4.st.entities.Subject;
+import sem4.st.model.SubjectModel;
+
+/**
+ * Servlet implementation class Subject
+ */
+@WebServlet("/SubjectServlet")
+public class SubjectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SubjectServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String action = request.getServletPath();
+
+		try {
+			switch (action) {
+			case "/SubjectServlet/new":
+				showNewForm(request, response);
+				break;
+			case "/SubjectServlet/insert":
+				insertSubject(request, response);
+				break;
+			case "/SubjectServlet/delete":
+				deleteSubject(request, response);
+				break;
+			case "/SubjectServlet/edit":
+				showEditForm(request, response);
+				break;
+			case "/SubjectServlet/update":
+				updateSubject(request, response);
+				break;
+			default:
+				listSubject(request, response);
+				break;
+			}
+		} catch (SQLException ex) {
+			throw new ServletException(ex);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	private void listSubject(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
+		List< Subject > listSubject = SubjectModel.listAllSubject();
+		request.setAttribute("listSubject", listSubject);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/SubjectList.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/FormSubject.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, ServletException, IOException {
+		String id = request.getParameter("id");
+		Subject existingSubject = SubjectModel.SearchById(id);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/FormSubject.jsp");
+		request.setAttribute("user", existingSubject);
+		dispatcher.forward(request, response);
+
+	}
+
+	private void insertSubject(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		String name = request.getParameter("name");
+		Subject newSubject = new Subject();
+		newSubject.setName(name);
+		SubjectModel.createSubject(newSubject);
+		response.sendRedirect("list");
+	}
+
+	private void updateSubject(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		long updatedAt = Long.parseLong(request.getParameter("updatedAt"));
+
+		Subject subject = new Subject();
+		subject.setId(id);
+		subject.setName(name);
+		subject.setUpdatedAt(updatedAt);
+		SubjectModel.updateSubject(subject);
+		response.sendRedirect("list");
+	}
+
+	private void deleteSubject(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		String id = request.getParameter("id");
+		SubjectModel.deleteSubject(id);
+		response.sendRedirect("list");
+
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/SubjectList.jsp
@@ -0,0 +1,40 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Subject Management</title>
+</head>
+<body>
+	<div align="center">
+		<h1>Subject Management</h1>
+		<h2>
+			<a href="new">Add New Subject</a> &nbsp;&nbsp;&nbsp; <a href="list">List
+				All Subject</a>
+
+		</h2>
+	</div>
+	<div align="center">
+		<table border="1" style="width: 100%;">
+			<caption>
+				<h2>List of Subjects</h2>
+			</caption>
+			<tr>
+				<th>ID</th>
+				<th>Name</th>
+			</tr>
+			<c:forEach var="subject" items="${listSubject}">
+				<tr>
+					<td><c:out value="${subject.id}" /></td>
+					<td><c:out value="${subject.name}" /></td>
+					<td><a href="edit?id=<c:out value='${subject.id}' />">Edit</a>
+						&nbsp;&nbsp;&nbsp;&nbsp; <a
+						href="delete?id=<c:out value='${subject.id}' />">Delete</a></td>
+				</tr>
+			</c:forEach>
+		</table>
+	</div>
+</body>
+</html>
